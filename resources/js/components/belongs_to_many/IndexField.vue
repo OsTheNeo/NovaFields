eac739cec565b578b5b4a19a09d2bd8f221b8888
hanaexcl <hanaexcl@gmail.com>
2023-05-22 17:17:38 +0800

    修改 max-w-xxs

--- resources/js/components/belongs_to_many/IndexField.vue
+++ resources/js/components/belongs_to_many/IndexField.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="max-w-xxs overflow-y-auto py-2">
+    <div class="overflow-y-auto py-2">
         <span v-for="(resource, key) in field.value" class="single">
             <Link v-if="field.viewable" :href="$url(`/resources/${field.resourceNameRelationship}/${resource.id}`)" class="link-default no-underline font-bold dim mr-1">
                     {{ get(resource, field.optionsLabel) + (key < field.value.length - 1 ? ', ' : '') }} </Link>
